c7e5a2b5af49a87605b0071ccf7c632fe804b0a9
Alan-Limyu-Exist <alan.limyu@exist.com>
2024-09-27 15:47:28 +0800

    Add CRUD API calls to Person and Role

--- core/src/main/java/com/exist/ecc/limyu_exercise8/core/controller/PersonController.java
+++ core/src/main/java/com/exist/ecc/limyu_exercise8/core/controller/PersonController.java
@@ -6,6 +6,8 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/person")
 public class PersonController {
@@ -13,8 +15,38 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
+    @GetMapping
+    public List<Person> getAll(
+            @RequestParam(value = "sortBy", required = false) String sortBy) {
+        if (sortBy == null) {
+            return personService.getAllPeople();
+        }
+
+        return switch (sortBy) {
+            case "gwa" -> personService.getAllPeopleByGwa();
+            case "lastName" -> personService.getAllPeopleByLastName();
+            case "dateHired" -> personService.getAllPeopleByDateHired();
+            default -> personService.getAllPeople();
+        };
+    }
+
     @PostMapping
     public Person create(@Valid @RequestBody Person person) {
         return personService.save(person);
+    }
+
+    @PutMapping("/{id}")
+    public Person update(@PathVariable long id, @Valid @RequestBody Person person) {
+        return personService.update(id, person);
+    }
+
+    @DeleteMapping
+    public void delete(@Valid @RequestBody Person person) {
+        personService.delete(person);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable long id) {
+        personService.deleteById(id);
     }
 }
--- core/src/main/java/com/exist/ecc/limyu_exercise8/core/controller/RoleController.java
+++ core/src/main/java/com/exist/ecc/limyu_exercise8/core/controller/RoleController.java
@@ -6,6 +6,8 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/role")
 public class RoleController {
@@ -13,8 +15,28 @@ public class RoleController {
     @Autowired
     private RoleService roleService;
 
+    @GetMapping
+    public List<Role> getAll() {
+        return roleService.getAllRoles();
+    }
+
     @PostMapping
     public Role create(@Valid @RequestBody Role role) {
         return roleService.save(role);
+    }
+
+    @PutMapping("/{id}")
+    public Role update(@PathVariable long id, @Valid @RequestBody Role role) {
+        return roleService.update(id, role);
+    }
+
+    @DeleteMapping
+    public void delete( @Valid @RequestBody Role role) {
+        roleService.delete(role);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable long id) {
+        roleService.deleteById(id);
     }
 }
